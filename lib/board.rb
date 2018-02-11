c96f1db4da259fc0d13ac05213b9c8857f0bbd25
Brian Ringley <brian.ringley@wework.com>
2018-02-11 19:47:22 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,8 @@
 board = []
 
-#I'm supposed to put 9 blank strings in the array but I refuse to type " " 9 times...
+# I'm supposed to put 9 blank strings in the array but I refuse to type " " 9 times...
+# So let's make the amount of strings a variable 
+num_positions = 9
 
-
+#I know I can do something n times using the n.times method
+num_positions.times 
